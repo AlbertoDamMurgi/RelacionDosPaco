fa48d407e08881d6cb645742f7b05f0537618192
AlbertoDamMurgi <fernandez.fernandez.angel@gmail.com>
2017-10-20 09:37:05 +0200

    Relaciondos Paco

--- Dropbox/Segundo DAM/PSP/ProyectosNetbeans PSP/Relaciondos/src/relaciondosbasica/Cuentaatras.java
+++ Dropbox/Segundo DAM/PSP/ProyectosNetbeans PSP/Relaciondos/src/relaciondosbasica/Cuentaatras.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package RelacionDosBasica;
+package relaciondosbasica;
 
 import java.awt.Color;
 import static java.lang.Thread.sleep;
