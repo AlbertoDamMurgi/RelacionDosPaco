82e8305a7062111dc3b06c6a9cc35dd5fcad7f62
AlbertoDamMurgi <fernandez.fernandez.angel@gmail.com>
2017-10-20 09:24:25 +0200

    Relaciondos Paco

---
+++ Dropbox/Segundo DAM/PSP/ProyectosNetbeans PSP/Relaciondos/src/RelacionDos/ClaseCompartir.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RelacionDos;
+
+/**
+ *
+ * @author usuario
+ */
+public class ClaseCompartir {
+    
+    private boolean parada;
+
+    public ClaseCompartir(boolean parada) {
+        this.parada = parada;
+    }
+
+    public boolean isParada() {
+        return parada;
+    }
+
+    public void setParada(boolean parada) {
+        this.parada = parada;
+    }
+    
+    
+    
+}
